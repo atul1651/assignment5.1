ba945790ac43943503edc17072a979195b651264
Atul Samage <samageatul@gmail.com>
2016-12-03 15:06:27 +0530

    javascript5.1

---
+++ app.js
@@ -0,0 +1,6 @@
+var name="Atul Samage";
+var age = 27;
+var placeofbirth="Kolhapur";
+var dateofbirth="10-3-1990";
+
+alert("your details are:" +" "+"Name:"+ name + " ," + " "+"Name:"+age + " ,"+ " "+"place of birth:" +placeofbirth + " ," +"Date of Birth "+dateofbirth)
